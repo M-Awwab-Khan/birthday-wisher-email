d36950ef07e6c464d0bb7757f1dcd840717c80b9
Awwab <mawwabkhank2006@gmail.com>
2024-02-07 10:26:33 +0500

    successfully completed birthday wisher

--- main.py
+++ main.py
@@ -3,11 +3,28 @@ import datetime as dt
 import smtplib
 import random
 import pandas as pd
-# 2. Check if today matches a birthday in the birthdays.csv
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+my_email = 'mawwabkhank2006@gmail.com'
+password = 'efcb xgpp xfgb waba'
 
-# 4. Send the letter generated in step 3 to that person's email address.
+data = pd.read_csv('birthdays.csv')
+now = dt.datetime.now()
+day = now.day
+month = now.month
+letter_filenames = ['./letter_templates/letter_1.txt', './letter_templates/letter_2.txt', './letter_templates/letter_3.txt']
+
+for row in data.itertuples():
+    if row[4] == month and row[5] == day:
+        with open(random.choice(letter_filenames), 'r') as f:
+            msg = f.read().replace('[NAME]', row[1])
+        with smtplib.SMTP('smtp.gmail.com') as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs=row[2],
+                msg=f'Subject:Happy Birthday {row[1]}\n\n{msg}'
+            )
 
 
 
