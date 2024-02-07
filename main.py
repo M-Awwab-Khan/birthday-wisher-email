61560b15937de9360ca1be7f5d83dff0ef8300ab
Awwab <mawwabkhank2006@gmail.com>
2024-02-07 10:14:25 +0500

    imported required modules

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 ##################### Extra Hard Starting Project ######################
-
-# 1. Update the birthdays.csv
-
+import datetime as dt
+import smtplib
+import random
+import pandas as pd
 # 2. Check if today matches a birthday in the birthdays.csv
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
