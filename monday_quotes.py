4d52bafba97a5a0e13342ab048f6ab86cbbf9771
Awwab <mawwabkhank2006@gmail.com>
2024-02-07 09:59:52 +0500

    send email with msg encoded in utf8

--- monday_quotes.py
+++ monday_quotes.py
@@ -9,3 +9,14 @@ with open('quotes.txt', 'r', encoding="utf8") as f:
     quotes = f.readlines()
 
 
+if dt.datetime.now().weekday() == 2:
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        random_quote = random.choice(quotes)
+        subject = random_quote.split('–')[-1].strip()
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs='docsofawwab@gmail.com',
+            msg=f'Subject:{subject}\n\n{random_quote}'.encode('utf-8')
+        )
